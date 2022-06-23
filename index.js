d8667fdc29f94fd8f96cab2657c84e7a3d7a875f
Moncef_Tighi <monceftighiouart@gmail.com>
2022-06-23 09:36:27 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import helmet from 'helmet';
+import createError from "http-errors";
+import cors from 'cors';
+
+import { errorHandeler } from "./controllers/errorController.js";
+// import produitsRouter from './routes/articlesRouter.js';
+// import tarifsRouter from './routes/tarifsRouter.js'
+// import viewRouter from './routes/viewRouter.js';
+ 
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.disable('x-powered-by');
+app.use(express.json({
+    limit : "100kb" //Limite la taille du body à 100kb
+}));
+app.use(express.urlencoded({extended: true}));
+
+
+app.use(express.static('public'));
+// app.use('/cegid/api/v1/articles', produitsRouter);
+// app.use('/cegid/api/v1/tarifs', tarifsRouter);
+// app.use('/cegid', viewRouter);
+
+app.all('*', (request, response, next)=> {    
+    //Ce middelware a pour seul but de catch les erreurs 404 
+    return next(createError(404, `Erreur 404 : Impossible de trouver l'URL ${request.originalUrl} sur ce serveur`))
+});
+
+app.use(errorHandeler);
+
+const port = process.env.PORT || 2000;
+
+app.listen(port, ()=> {
+    console.log(`Server ouvert sur le port ${port}`);
+});
